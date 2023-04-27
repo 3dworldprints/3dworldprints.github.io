3645a0ca8b16f672552b64c56604b44c67a5c5d6
3dworldprints <131779597+3dworldprints@users.noreply.github.com>
2023-04-27 22:21:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-const cartIcon = document.querySelector('.cart');
-const cartItemsList = document.querySelector('.cart-items-list');
-const cartItemsUl = document.querySelector('.cart-items-list ul');
-const checkoutBtn = document.querySelector('.checkout');
-const productBtns = document.querySelectorAll('.add-to-cart');
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
 
-let cartItems = [];
-
-function displayCartItems() {
-    cartItemsUl.innerHTML = '';
-
-    cartItems.forEach(item => {
-        const li = document.createElement('li');
-        li.innerText = `${item.name} - ${item.price}$`;
-        cartItemsUl.appendChild(li);
-    });
-
-    if (cartItems.length === 0) {
-        const li = document.createElement('li');
-        li.innerText = 'No hay productos en el carrito.';
-        cartItemsUl.appendChild(li);
-    }
-}
-
-function toggleCartItemsList() {
-    cartItemsList.classList.toggle('show');
-}
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', addToCart);
+});
 
 function addToCart(event) {
     const button = event.target;
@@ -32,33 +10,34 @@ function addToCart(event) {
     const name = button.getAttribute('data-name');
     const price = button.getAttribute('data-price');
 
-    const item = {
-        id,
-        name,
-        price
-    };
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-    cartItems.push(item);
+    let item = cart.find(item => item.id === id);
+    if (!item) {
+        item = {
+            id,
+            name,
+            price,
+            quantity: 1
+        };
+        cart.push(item);
+    } else {
+        item.quantity++;
+    }
 
-    displayCartItems();
-    updateCartItemsCount();
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    updateCartCount();
 }
 
-function updateCartItemsCount() {
-    const cartItemsCount = document.querySelector('.cart-items');
-    cartItemsCount.innerText = cartItems.length;
+function updateCartCount() {
+    const cartCount = document.getElementById('cart-count');
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    let count = 0;
+    cart.forEach(item => {
+        count += item.quantity;
+    });
+
+    cartCount.innerText = count;
 }
-
-cartIcon.addEventListener('click', toggleCartItemsList);
-
-productBtns.forEach(button => {
-    button.addEventListener('click', addToCart);
-});
-
-checkoutBtn.addEventListener('click', () => {
-    alert('¡Gracias por su compra!');
-});
-
-updateCartItemsCount();
-displayCartItems();
-
